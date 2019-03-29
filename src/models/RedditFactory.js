b3f26d360ceeea407458a5f40edfef3d6dacc08a
George <georgej1088@gmail.com>
2019-03-29 09:59:42 -0400

    Convert RedditFactory from class to plain object
    With no instance methods or constructor, no reason to use a class for this

--- src/models/RedditFactory.js
+++ src/models/RedditFactory.js
@@ -4,25 +4,17 @@ export const USER_AGENT = process.env.VUE_APP_USER_AGENT;
 export const CLIENT_ID = process.env.VUE_APP_CLIENT_ID;
 export const REDIRECT_URL = process.env.VUE_APP_REDIRECT_URL;
 
-export default class RedditFactory {
+export default {
   /** @member {snoowrap|null} _redditInstance */
-  static _redditInstance = null;
+  _redditInstance: null,
 
-  constructor() {
-    throw TypeError('Use instance()');
-  }
-
-  /**
-     * @returns {snoowrap|null}
-     */
-  static instance() {
+  /** @returns {snoowrap|null} */
+  instance() {
     return this._redditInstance;
-  }
+  },
 
-  /**
-     * @param reddit - A snoowrap Reddit instance.
-     */
-  static setReddit(reddit) {
+  /** @param reddit A snoowrap Reddit instance. */
+  setReddit(reddit) {
     if (!(reddit instanceof snoowrap)) {
       throw new TypeError('Parameter must be a snoowrap instance.')
     }
