b34ac68bd329e2c41c97809a8b6ba524bf1f0500
Eric Gustavsson <egustavs@redhat.com>
2019-04-10 17:58:50 +0200

    feat(ContentListQueue): Added VuePullToReload
    No Vue pull to refresh was good enough for our project. We're will be working on the VuePullToReload
    until it is ready.
    
    re #18

--- src/views/partials/ContentListQueue.vue
+++ src/views/partials/ContentListQueue.vue
@@ -1,44 +1,48 @@
 <template>
   <v-container fluid>
-    <v-layout
-      row
-      wrap
-    >
-      <v-flex
-        v-for="list in listing"
-        :key="list.id"
-        xs12
-        my3
+    <pull-to-reload :top-load-method="() => {}">
+      <v-layout
+        row
+        wrap
       >
-        <v-container
-          fluid
-          pa-0
-          pb-2
-          ma-0
+        <v-flex
+          v-for="list in listing"
+          :key="list.id"
+          xs12
+          my3
         >
-          <ms-submission
-            v-if="list.constructor._name === 'Submission'"
-            :submission="list"
-          />
-          <ms-comment
-            v-else
-            :comment="list"
-          />
-        </v-container>
-      </v-flex>
-    </v-layout>
+          <v-container
+            fluid
+            pa-0
+            pb-2
+            ma-0
+          >
+            <ms-submission
+              v-if="list.constructor._name === 'Submission'"
+              :submission="list"
+            />
+            <ms-comment
+              v-else
+              :comment="list"
+            />
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </pull-to-reload>
   </v-container>
 </template>
 
 <script>
 import MsSubmission from '../../components/Submission';
 import MsComment from '../../components/Comment';
+import PullToReload from 'vue-pull-to-reload';
     
 export default {
   name: 'MsContentListQueue',
   components: {
     MsSubmission,
     MsComment,
+    PullToReload,
   },
   props: {
     listing: {
